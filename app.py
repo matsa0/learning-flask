795bda0f586546e671d4b50339a6a824caffffa1
Matheus Azevedo <matheus.azevedo@aluno.ufop.edu.br>
2023-11-28 00:21:34 -0300

    Adicionando dados dinâmicos em lista e dicionário(requisição POST)

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask, render_template #para retornar o template HTML da pasta templates
-
-#setx FLASK_ENV development
-#flask run
+from flask import Flask, render_template, request
 
 
 #criando a aplicação Flask
@@ -9,18 +6,34 @@ app = Flask(__name__)
 
 
 #criando a rota principal para rodar a aplicação
-@app.route('/')
+frutas = []
+registros = []
+
+@app.route('/', methods=["GET", "POST"]) #aceita solicitações GET e POST
 def main():
-    nome = "Matheus"
-    idade = 20
-    return render_template("index.html", nome=nome, idade=idade) #o primeiro nome é uma variável
+    if request.method == "POST": #verifica se a requsição feita é do tipo POST, ou seja, se o usuário submeteu o formulário
+        if request.form.get("fruta"): #verifica se há algum dado enviado pelo formulário name
+            frutas.append(request.form.get("fruta"))
+    return render_template("index.html", frutas=frutas) #o primeiro nome é uma variável
 
 
+    
+'''
+   Quando um formulário é submetido por método POST, verifica-se se há dados válidos para "aluno" e "nota" no formulário. 
+   Se sim, esses dados são usados para criar um novo dicionário contendo as chaves "aluno" e "nota" e seus respectivos valores. 
+   Esse dicionário é então adicionado à lista registros.
+'''
 #criando outra rota(url) com sobre
-@app.route('/sobre') 
+@app.route('/sobre', methods=["GET", "POST"]) 
 def sobre():
-    return render_template("sobre.html") 
+    if request.method == "POST":
+        if request.form.get("aluno") and request.form.get("nota"):
+            registros.append({"aluno": request.form.get("aluno"), "nota": request.form.get("nota")}) 
+
+
+
+    return render_template("sobre.html", registros=registros) 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) #rodar a aplicação com debug=true e invés de utilizar 'flask run', 'pyhton3 app.py'
     
